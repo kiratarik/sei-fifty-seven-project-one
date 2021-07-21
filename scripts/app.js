96cf342a05e9e70560c678eee37f67e88f4fa391
Tarik Canessa-Wright <tarik.canessa.wright@gmail.com>
2021-07-21 17:03:54 +0100

    Rotations complete + changed to global variables

--- scripts/app.js
+++ scripts/app.js
@@ -11,11 +11,7 @@ const statisticsScores = []
 // const shapeJString = 'shape-j'
 // const shapeIString = 'shape-i'
 const gameSpeedTime = 100
-let currentShapePosition = 0
-let currentShapeString = ''
-let currentPositionModifiers = []
 let gameOver = false
-const gameWidth = 10 + 2
 let gridWidth = 7
 
 // * Objects
@@ -24,7 +20,6 @@ const currentShape = {
   nameId: 0,
   nameString: 'shape-?',
   position: 0,
-  rotation: 0,
   positionModifiers: [0, 0, 0, 0],
 }
 
@@ -55,21 +50,6 @@ class shapeLibrary {
       [-2, -gridWidth * 2, 2, gridWidth * 2]
     ]
   }
-  rotateCurrent() {
-    const oldModifiers = currentShape.positionModifiers
-    const newModifiers = []
-    for (let indexOne = 0; indexOne < 4; indexOne++) {
-      for (let index = 0; index < 4; index++) {
-        const rotations = this.rotationTranslations[indexOne]
-        const rotation = rotations[index]
-        if (oldModifiers[indexOne] === rotation) {
-          newModifiers.push(rotations[(index + currentShape.rotation) % 4])
-          index = 4
-        }
-      }
-    }
-    currentShape.positionModifiers = newModifiers
-  }
 }
 
 
@@ -77,7 +57,7 @@ class shapeLibrary {
 
 // * Functions
 // Build Grids
-function buildGrid(gridWidth, gridHeight, gridSelect, cellSize) {
+function buildGrid(gridHeight, gridSelect, cellSize) {
   cells.length = 0
   for (let index = 0; index < gridWidth * gridHeight; index++) {
     const cell = document.createElement('div')
@@ -94,29 +74,30 @@ function buildGrid(gridWidth, gridHeight, gridSelect, cellSize) {
 }
 
 function buildStats(width, gridHeight, statsSelect, cellSize) {
+  // build grid
   gridWidth = width
-  buildGrid(gridWidth, gridHeight, statsSelect, cellSize)
+  buildGrid(gridHeight, statsSelect, cellSize)
+  
+  // Set Shape Statistics Score display
   for (let index = ((2 * gridWidth) - 1); index < (gridWidth * gridHeight); index += (3 * gridWidth)) {
     statisticsScores.push(cells[index])
-    cells[index].textContent = '0'
+    cells[index].textContent = '-1'
     cells[index].classList.add('stats-number')
   }
+  
+  // build shapes
   for (let index = 0; index < 7; index++) {
-    const newPosition = ((((index * 3) + 2) * gridWidth) - gridWidth + 2)
-    buildStatsShape(newPosition, index)
+    currentShape.position = ((((index * 3) + 2) * gridWidth) - gridWidth + 2)
+    currentShape.nameId = index
+    buildGameShape()
   }
-  // buildShapeT(gridWidth, ((((0 * 3) + 2) * gridWidth) - gridWidth + 2))
-  // buildShapeL(gridWidth, ((((1 * 3) + 2) * gridWidth) - gridWidth + 2))
-  // buildShapeS(gridWidth, ((((2 * 3) + 2) * gridWidth) - gridWidth + 2))
-  // buildShapeO(gridWidth, ((((3 * 3) + 2) * gridWidth) - gridWidth + 2))
-  // buildShapeZ(gridWidth, ((((4 * 3) + 2) * gridWidth) - gridWidth + 2))
-  // buildShapeJ(gridWidth, ((((5 * 3) + 2) * gridWidth) - gridWidth + 2))
-  // buildShapeI(gridWidth, ((((6 * 3) + 2) * gridWidth) - gridWidth + 2))
-  currentShapeString = ''
+  currentShape.nameString = ''
 }
 
-function buildGame(gridWidth, gridHeight, gridSelect, cellSize) {
-  buildGrid(gridWidth, gridHeight, gridSelect, cellSize)
+function buildGame(width, height, gridSelect, cellSize) {
+  gridWidth = width + 2
+  const gridHeight = height + 2
+  buildGrid(gridHeight, gridSelect, cellSize)
   for (let index = 0; index < gridWidth * gridHeight; index += gridWidth) {
     cells[index].classList.add('wall')
     cells[index + gridWidth - 1].classList.add('wall')
@@ -129,165 +110,176 @@ function buildGame(gridWidth, gridHeight, gridSelect, cellSize) {
 }
 
 // Build Shapes
-function buildStatsShape(position, shapeId) {
-  const statsLibrary = new shapeLibrary(gridWidth)
-  buildGamePiece(position, statsLibrary.nameStrings[shapeId], statsLibrary.basePositions[shapeId])
+function buildGameShape() {
+  const gameLibrary = new shapeLibrary(gridWidth)
+  statisticsScores[currentShape.nameId].textContent = parseInt(statisticsScores[currentShape.nameId].textContent) + 1
+  currentShape.nameString = gameLibrary.nameStrings[currentShape.nameId]
+  currentShape.positionModifiers = gameLibrary.basePositions[currentShape.nameId]
+  buildGamePiece()
 }
-function buildGameShape(position, shapeId) {
-  const gameLibrary = new shapeLibrary(gameWidth)
-  statisticsScores[shapeId].textContent = parseInt(statisticsScores[shapeId].textContent) + 1
-  currentShapeString = gameLibrary.nameStrings[shapeId]
-  currentPositionModifiers = gameLibrary.basePositions[shapeId]
-  buildGamePiece(position, gameLibrary.nameStrings[shapeId], gameLibrary.basePositions[shapeId])
-}
-// function buildShapeT(gridWidth, position) {
-//   currentShapeString = shapeTString
-//   currentPositionModifiers = [0, -1, 1, -gridWidth]
-//   statisticsScores[0].textContent = parseInt(statisticsScores[0].textContent) + 1
-//   buildGamePiece(position, currentShapeString, currentPositionModifiers)
-// }
-// function buildShapeL(gridWidth, position) {
-//   currentShapeString = shapeLString
-//   currentPositionModifiers = [0, -1, 1, -gridWidth + 1]
-//   statisticsScores[1].textContent = parseInt(statisticsScores[1].textContent) + 1
-//   buildGamePiece(position, currentShapeString, currentPositionModifiers)
-// }
-// function buildShapeS(gridWidth, position) {
-//   currentShapeString = shapeSString
-//   currentPositionModifiers = [0, -1, -gridWidth, -gridWidth + 1]
-//   statisticsScores[2].textContent = parseInt(statisticsScores[2].textContent) + 1
-//   buildGamePiece(position, currentShapeString, currentPositionModifiers)
-// }
-// function buildShapeO(gridWidth, position) {
-//   currentShapeString = shapeOString
-//   currentPositionModifiers = [0, 1, -gridWidth, -gridWidth + 1]
-//   statisticsScores[3].textContent = parseInt(statisticsScores[3].textContent) + 1
-//   buildGamePiece(position, currentShapeString, currentPositionModifiers)
-// }
-// function buildShapeZ(gridWidth, position) {
-//   currentShapeString = shapeZString
-//   currentPositionModifiers = [0, 1, -gridWidth, -gridWidth - 1]
-//   statisticsScores[4].textContent = parseInt(statisticsScores[4].textContent) + 1
-//   buildGamePiece(position, currentShapeString, currentPositionModifiers)
-// }
-// function buildShapeJ(gridWidth, position) {
-//   currentShapeString = shapeJString
-//   currentPositionModifiers = [0, -1, 1, -gridWidth - 1]
-//   statisticsScores[5].textContent = parseInt(statisticsScores[5].textContent) + 1
-//   buildGamePiece(position, currentShapeString, currentPositionModifiers)
-// }
-// function buildShapeI(gridWidth, position) {
-//   currentShapeString = shapeIString
-//   currentPositionModifiers = [0, -1, 1, 2]
-//   statisticsScores[6].textContent = parseInt(statisticsScores[6].textContent) + 1
-//   buildGamePiece(position, currentShapeString, currentPositionModifiers)
-// }
-function buildGamePiece(position, currentShapeString, currentPositionModifiers) {
-  if (checkCollision(position, currentPositionModifiers)) {
+
+function buildGamePiece() {
+  if (checkCollision()) {
     gameOver = true
   } else {
-    buildShapeColor(position, currentShapeString, currentPositionModifiers)
+    buildShapeColor()
   }
 }
 
-function buildShapeColor(position, currentShapeString, currentPositionModifiers) {
-  currentPositionModifiers.forEach(modifier => {
-    cells[position + modifier].classList.add(currentShapeString)
+function buildShapeColor() {
+  currentShape.positionModifiers.forEach(modifier => {
+    cells[currentShape.position + modifier].classList.add(currentShape.nameString)
   })
 }
-function deleteShapeColor(position, currentShapeString, currentPositionModifiers) {
-  currentPositionModifiers.forEach(modifier => {
-    cells[position + modifier].classList.remove(currentShapeString)
+function deleteShapeColor() {
+  currentShape.positionModifiers.forEach(modifier => {
+    cells[currentShape.position + modifier].classList.remove(currentShape.nameString)
   })
 }
-function buildShapeRandom(gridWidth, position) {
-  const randomShapeNum = Math.floor(Math.random() * 7)
-  buildGameShape(position, randomShapeNum)
-  // switch (randomShapeNum) {
-  //   case 0:
-  //     buildShapeT(gridWidth, position)
-  //     break
-  //   case 1:
-  //     buildShapeL(gridWidth, position)
-  //     break
-  //   case 2:
-  //     buildShapeS(gridWidth, position)
-  //     break
-  //   case 3:
-  //     buildShapeO(gridWidth, position)
-  //     break
-  //   case 4:
-  //     buildShapeZ(gridWidth, position)
-  //     break
-  //   case 5:
-  //     buildShapeJ(gridWidth, position)
-  //     break
-  //   case 6:
-  //     buildShapeI(gridWidth, position)
-  //     break
-  // }
-  currentShapePosition = position
+function buildShapeRandom() {
+  currentShape.nameId = Math.floor(Math.random() * 7)
+  buildGameShape()
   console.log('build Shape')
 }
 
-function checkCollision(position, currentPositionModifiers) {
-  return (currentPositionModifiers.filter(modifier => {
-    return cells[position + modifier].classList.length !== 0
+function checkCollision() {
+  return (currentShape.positionModifiers.filter(modifier => {
+    return cells[currentShape.position + modifier].classList.length !== 0
   }).length)
 }
 
-function shapeFall(gridWidth, position) {
-  deleteShapeColor(position, currentShapeString, currentPositionModifiers)
-  if (!checkCollision(position + gridWidth, currentPositionModifiers)) {
-    currentShapePosition = position + gridWidth
-    buildShapeColor(currentShapePosition, currentShapeString, currentPositionModifiers)
+function shapeFall() {
+  deleteShapeColor()
+  currentShape.position = currentShape.position + gridWidth
+  if (!checkCollision()) {
+    buildShapeColor()
   } else {
-    buildShapeColor(position, currentShapeString, currentPositionModifiers)
-    buildShapeRandom(gridWidth, 17)
+    currentShape.position = currentShape.position - gridWidth
+    buildShapeColor()
+    currentShape.position = 17
+    buildShapeRandom()
   }
 }
 
 function rotateClockwise() {
-  currentShape.rotation = (currentShape.rotation + 1) % 4
-  const gameLibrary = new shapeLibrary(gameWidth)
-  gameLibrary.rotateCurrent()
+  // delete current shape colors
+  deleteShapeColor()
+
+  // make 1 clockwise rotation and check collision
+  rotateCurrent(1)
+  if (!checkCollision()) {
+    // color it
+    buildShapeColor()
+  } else {
+    // move it back and color it
+    rotateCurrent(-1)
+    buildShapeColor()
+  }
+}
+function rotateCounterclockwise() {
+  // delete current shape colors
+  deleteShapeColor()
+
+  // make 1 clockwise rotation and check collision
+  rotateCurrent(-1)
+  if (!checkCollision()) {
+    // color it
+    buildShapeColor()
+  } else {
+    // move it back and color it
+    rotateCurrent(1)
+    buildShapeColor()
+  }
+}
+
+function rotateCurrent(direction) {
+  const gameLibrary = new shapeLibrary(gridWidth)
+  const oldModifiers = currentShape.positionModifiers
+  const newModifiers = []
+  for (let indexTwo = 0; indexTwo < 4; indexTwo++) {
+    for (let indexOne = 0; indexOne < 4; indexOne++) {
+      for (let index = 0; index < 4; index++) {
+        const rotations = gameLibrary.rotationTranslations[indexOne]
+        const rotation = rotations[index]
+        if (oldModifiers[indexTwo] === rotation) {
+          newModifiers.push(rotations[(index + direction + 4) % 4])
+          index = 4
+        }
+      }
+    }
+  }
+  currentShape.positionModifiers = newModifiers
 }
 
 function handleKeyDown(event) {
-  switch (event.which) {
+  // If game is over don't accept following button presses
+  if (gameOver === true) {
+    return
+  }
+
+  // look at which key was pressed
+  switch (event.which || event.keyCode) {
     case 37:
+      // Left Arrow Key
       handleLeft()
       break
     case 39:
+      // Right Arrow Key
       handleRight()
+      break
+    case 38:
+      // Up Arrow Key
+      rotateClockwise()
+      break
+    case 40:
+      // Up Arrow Key
+      rotateCounterclockwise()
       break
   }
 }
 
 function handleLeft() {
-  deleteShapeColor(currentShapePosition, currentShapeString, currentPositionModifiers)
-  if (!checkCollision(currentShapePosition - 1, currentPositionModifiers)) {
-    currentShapePosition--
-    buildShapeColor(currentShapePosition, currentShapeString, currentPositionModifiers)
+  // delete current shape colors
+  deleteShapeColor()
+
+  // move 1 tile left and check collision
+  currentShape.position--
+  if (!checkCollision()) {
+    // color it
+    buildShapeColor()
   } else {
-    buildShapeColor(currentShapePosition, currentShapeString, currentPositionModifiers)
+    // move it back and color it
+    currentShape.position++
+    buildShapeColor()
   }
 }
 function handleRight() {
-  deleteShapeColor(currentShapePosition, currentShapeString, currentPositionModifiers)
-  if (!checkCollision(currentShapePosition + 1, currentPositionModifiers)) {
-    currentShapePosition++
-    buildShapeColor(currentShapePosition, currentShapeString, currentPositionModifiers)
+  // delete current shape colors
+  deleteShapeColor()
+
+  // move 1 tile right and check collision
+  currentShape.position++
+  if (!checkCollision()) {
+    // color it
+    buildShapeColor()
   } else {
-    buildShapeColor(currentShapePosition, currentShapeString, currentPositionModifiers)
+    // move it back and color it
+    currentShape.position--
+    buildShapeColor()
   }
 }
 
+function startUp() {
+  gridWidth = 7
+  buildStats(7, 21, document.querySelector('#statistics'), 4)
+  gridWidth = 12
+  buildGame(10, 20, document.querySelector('#board'), 4)
+  currentShape.position = 17
+  buildShapeRandom()
+}
 
 // * Create Screen
-buildStats(7, 21, document.querySelector('#statistics'), 4)
-buildGame(gameWidth, 20 + 2, document.querySelector('#board'), 4)
-buildShapeRandom(gameWidth, 17)
+startUp()
 
 
 
@@ -298,7 +290,7 @@ window.addEventListener('keydown', handleKeyDown)
 
 // Intervals
 const gameIntervalId = setInterval(function(){
-  shapeFall(12, currentShapePosition)
+  shapeFall()
   console.log('fall')
   if (gameOver) {
     console.log('Game Over')
